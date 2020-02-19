8d88ce15c83ce9c4f711cdc6ff89cc00e238af10
ebueeno <erickbueno_@hotmail.com>
2020-02-19 14:21:50 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,15 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Book");
 		
-		Seller seller = new Seller(21,"Bob","Bob@gmail.com",new Date(), 3000.00,obj);
+				
+		
 		
 		SellerDao sellerDao = DaoFacory.creatSellerDao();
 		
-		System.out.println(seller);
+		Seller seller = sellerDao.findById(3);
+		
+		System.err.println(seller);
 		
 		
 		
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -16,6 +16,10 @@ public class Department implements Serializable {
 		this.name = name;
 	}
 
+	public Department() {
+		
+	}
+
 	public Integer getId() {
 		return id;
 	}
